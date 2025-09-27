bf0cd741e7a2c9fbf0a6bf67808d0959f3d624be
CoderZENNXX <brocodestudent@gmail.com>
2025-09-27 22:27:33 +0700

    Project V8

--- src/JSX/AboutUs.jsx
+++ src/JSX/AboutUs.jsx
@@ -1,9 +1,28 @@
+import "../CSS/AboutUs.css"
+
 function AboutUs() {
     return (
-        <div className="about-us">
-            
+        <div className="about-us-page">
+            <h1 className="about-us-title">
+                About Us
+            </h1>
+            <div className="about-us-content">
+                <h1 className="origin-missions-text">Origin and missions: Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel exercitationem rerum dolore voluptates reprehenderit impedit fugit, eaque nemo beatae dolorum illo expedita dolorem ipsum ipsam non magni inventore mollitia optio Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nam minima veritatis veniam, facilis beatae sunt consectetur culpa, totam perferendis, fugiat explicabo autem impedit in maxime quisquam quos aliquam animi..</h1>
+                <h1 className="food-text">Food: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat distinctio quo incidunt quia sapiente expedita, velit dolor odio aut modi cum exercitationem minus aspernatur odit sunt magnam quibusdam libero voluptatem.lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Id alias repellat illo vel natus laudantium mollitia corporis et, aspernatur quae distinctio harum amet possimus vero facere tenetur libero quisquam! Repudiandae.</h1>
+                <div className="about-us-image-container">
+                    <img src="./about-us-1.jpg" className="about-us-1-image" />
+                    <img src="./about-us-2.jpg" className="about-us-2-image" />
+                    <img src="./about-us-3.jpg" className="about-us-3-image" />
+                    <img src="./about-us-4.jpg" className="about-us-4-image" />
+                    <img src="./about-us-5.jpg" className="about-us-5-image" />
+                    <img src="./about-us-6.jpg" className="about-us-6-image" />
+                    <img src="./about-us-7.jpg" className="about-us-7-image" />
+                    <img src="./about-us-8.jpg" className="about-us-8-image" />
+                    <img src="./about-us-9.jpg" className="about-us-9-image" />
+                    <img src="./about-us-10.jpg" className="about-us-10-image" />
+                </div>
+            </div>
         </div>
     )
 }
-
 export default AboutUs
